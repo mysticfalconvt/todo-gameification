af8c298d34b892cf3d499ffd004e03a88288f8e0
Rob Boskind <49008703+mysticfalconvt@users.noreply.github.com>
2026-04-18 12:42:42 -0400

    styuling build bug

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -30,6 +30,12 @@ export const Route = createRootRoute({
       { title: 'Todo Gameification' },
     ],
     links: [
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500;9..144,700&family=Manrope:wght@400;500;600;700;800&display=swap',
+      },
       { rel: 'stylesheet', href: appCss },
       { rel: 'manifest', href: '/manifest.json' },
       { rel: 'icon', href: '/favicon.ico' },
